0ebe2ccdb2b1b641fe6b4d19fec29b83253b696d
Lorenzo Giuntini <lorenzogi@akeron.local>
2020-03-30 22:00:29 +0200

    Angular first commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'), 
+    ts = require('gulp-typescript'), 
+    exec = require('child_process').exec, 
+    nodemon = require('gulp-nodemon'); 
+
+gulp.task('backend', function () { 
+    return gulp.src('./server/**/*.ts')
+        .pipe(ts({ noImplicitAny: true, lib: ["es2015"] 
+    }))
+    .pipe(gulp.dest('dist/')); 
+});
+
+gulp.task('frontend', function (cb) { 
+    exec('npx ng build --prod --outputPath=dist/public', function (err, stdout, stderr) { 
+        console.log(stdout); 
+        console.log(stderr); 
+        cb(err); 
+    }); 
+})
+
+
+//gulp.task('watch', ['backend'], function () {
+  //  gulp.watch('./server/**/*.ts', ['backend']);
+//});
+
+gulp.task('nodemon', function () { 
+    nodemon({ 
+        script: 'dist/server.js', 
+        ext: 'js', 
+        env: { 'NODE_ENV': 'development' } 
+    }) 
+});
+
+//gulp.task('default', ['frontend', 'backend', 'watch', 'nodemon']);
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app';
+
+  constructor(httpClient: HttpClient) {
+
+    httpClient.get<string>('http://localhost:3000/api/test')
+      .subscribe(arg => this.title = arg);
+
+  }
+};
