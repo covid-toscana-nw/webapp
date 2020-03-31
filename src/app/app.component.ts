9e868766506eceae8cf934fa5e5b7bbd8b22a99d
Lorenzo Giuntini <lorenzogi@akeron.local>
2020-03-31 15:25:04 +0200

    add charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -14,4 +14,279 @@ export class AppComponent {
     httpClient.get<string>('http://localhost:3000/api/test')
       .subscribe(arg => this.title = arg);
   }
-};
+};*/
+
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+
+  selector: 'app-root',
+
+  templateUrl: './app.component.html',
+
+  styleUrls: ['./app.component.scss']
+
+})
+
+export class AppComponent {
+
+  title = 'app';
+
+  constructor(httpClient: HttpClient) {
+
+    httpClient.get<string>('http://localhost:3000/api/test')
+      .subscribe(arg => this.title = arg);
+  }
+
+  onSelect(event) {
+    console.log(event);
+  };
+
+  view: any[] = [600, 400];
+
+  // options for the chart
+
+  showXAxis = true;
+
+  showYAxis = true;
+
+  gradient = false;
+
+  showLegend = true;
+
+  showXAxisLabel = true;
+
+  xAxisLabel = 'Country';
+
+  showYAxisLabel = true;
+
+  yAxisLabel = 'Sales';
+
+  timeline = true;
+
+  colorScheme = {
+
+    domain: ['#9370DB', '#87CEFA', '#FA8072', '#FF7F50', '#90EE90', '#9370DB']
+
+  };
+
+  //pie
+
+  showLabels = true;
+
+  // data goes here
+
+public single = [
+
+  {
+
+    "name": "China",
+
+    "value": 2243772
+
+  },
+
+  {
+
+    "name": "USA",
+
+    "value": 1126000
+
+  },
+
+  {
+
+    "name": "Norway",
+
+    "value": 296215
+
+  },
+
+  {
+
+    "name": "Japan",
+
+    "value": 257363
+
+  },
+
+  {
+
+    "name": "Germany",
+
+    "value": 196750
+
+  },
+
+  {
+
+    "name": "France",
+
+    "value": 204617
+
+  }
+
+];
+
+public multi = [
+
+  {
+
+    "name": "China",
+
+    "series": [
+
+      {
+
+        "name": "2018",
+
+        "value": 2243772
+
+      },
+
+      {
+
+        "name": "2017",
+
+        "value": 1227770
+
+      }
+
+    ]
+
+  },
+
+  {
+
+    "name": "USA",
+
+    "series": [
+
+      {
+
+        "name": "2018",
+
+        "value": 1126000
+
+      },
+
+      {
+
+        "name": "2017",
+
+        "value": 764666
+
+      }
+
+    ]
+
+  },
+
+  {
+
+    "name": "Norway",
+
+    "series": [
+
+      {
+
+        "name": "2018",
+
+        "value": 296215
+
+      },
+
+      {
+
+        "name": "2017",
+
+        "value": 209122
+
+      }
+
+    ]
+
+  },
+
+  {
+
+    "name": "Japan",
+
+    "series": [
+
+      {
+
+        "name": "2018",
+
+        "value": 257363
+
+      },
+
+      {
+
+        "name": "2017",
+
+        "value": 205350
+
+      }
+
+    ]
+
+  },
+
+  {
+
+    "name": "Germany",
+
+    "series": [
+
+      {
+
+        "name": "2018",
+
+        "value": 196750
+
+      },
+
+      {
+
+        "name": "2017",
+
+        "value": 129246
+
+      }
+
+    ]
+
+  },
+
+  {
+
+    "name": "France",
+
+    "series": [
+
+      {
+
+        "name": "2018",
+
+        "value": 204617
+
+      },
+
+      {
+
+        "name": "2017",
+
+        "value": 149797
+
+      }
+
+    ]
+
+  }
+
+];
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-  
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-
 @NgModule({
+
   declarations: [
     AppComponent
   ],
+
   imports: [
-    BrowserModule, HttpClientModule
+
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    NgxChartsModule,
+    BrowserAnimationsModule
+
   ],
+
   providers: [],
+
   bootstrap: [AppComponent]
+
 })
-export class AppModule { };
+
+export class AppModule { }
