bfd491ee00b51e19f60c3d976a3334d632d438af
Michele Carignani <michele.carignani@gmail.com>
2020-04-28 19:46:50 +0200

    migrate to new github organization

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  dataSourceUrl: 'https://raw.githubusercontent.com/michele-carignani/bollettini-toscana-nw-datasets/master/toscana_nw_all_datasets.json'
+  dataSourceUrl: 'https://raw.githubusercontent.com/covid-toscana-nw/dataset/master/toscana_nw_all_datasets.json'
 };
